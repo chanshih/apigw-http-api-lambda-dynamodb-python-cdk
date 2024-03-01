32420e3dd98cfe8246f0981b6ef4d6b4e889e4c9
EC2 Default User <ec2-user@ip-172-31-16-8.ap-southeast-2.compute.internal>
2024-03-01 08:49:51 +0000

    cross acc deployment

--- stacks/pipeline_stage.py
+++ stacks/pipeline_stage.py
@@ -1,14 +1,13 @@
 from constructs import Construct
 from aws_cdk import (
     Stage,
-    Tags,
-    Environment
+    Tags
 )
 from stacks.apigw_http_api_lambda_dynamodb_python_cdk_stack import ApigwHttpApiLambdaDynamodbPythonCdkStack
 
 class PipelineStage(Stage):
 
-    def __init__(self, scope: Construct, id: str, env=Environment, **kwargs):
+    def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
         stack = ApigwHttpApiLambdaDynamodbPythonCdkStack(self, 'ApigwHttpApiLambdaDynamodbPythonCdkStack', stage=str(id))
